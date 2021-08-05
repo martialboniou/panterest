68425806aa0843aa2d9abf56172f906cc7fe4295
martial-boniou <martial.boniou2021@campus-eni.fr>
2021-08-05 13:43:56 +0200

    add show page and navigation

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Pin;
 use App\Repository\PinRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,4 +17,12 @@ class PinsController extends AbstractController
         return $this->render('pins/index.html.twig',
             compact('pins'));
     }
+
+    #[Route('/pins/{id<[0-9]+>}', name: 'app_pins_show')]
+    public function show(Pin $pin): Response
+    {
+        return $this->render('pins/show.html.twig',
+            compact('pin'));
+    }
+
 }
