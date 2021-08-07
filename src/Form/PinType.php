977976b8b239535fc0fbe4c9453bf5222256147b
martial-boniou <martial.boniou2021@campus-eni.fr>
2021-08-07 12:59:34 +0200

    uploads

--- src/Form/PinType.php
+++ src/Form/PinType.php
@@ -7,12 +7,20 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class PinType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('imageFile', VichImageType::class, [
+                'required' => false,
+                'label' => 'Image (JPG or PNG file)',
+                'allow_delete' => true,
+                'download_label' => '...',
+                'download_uri' => false,
+            ])
             ->add('title')
             ->add('description')
         ;
