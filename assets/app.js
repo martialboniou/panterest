b2f59e4f9e7ddd23bcc322f2568be6d943f4a269
martial-boniou <martial.boniou2021@campus-eni.fr>
2021-08-07 15:50:22 +0200

    styling of img thumbnail with scss

--- assets/app.js
+++ assets/app.js
@@ -9,4 +9,19 @@
 import './styles/scss/app.scss';
 
 // You can specify which plugins you need
-import { Tooltip, Toast, Popover } from 'bootstrap';
+import {Tooltip, Toast, Popover} from 'bootstrap';
+
+document.addEventListener('DOMContentLoaded',
+    function () {
+        /*
+        const $fileInputs = document.getElementsByClassName('custom-file-input');
+        if ($fileInputs.length > 0) {
+            $fileInputs[0].addEventListener('change',
+                function(event) {
+                    const $input = event.currentTarget;
+                    const $label = $input.parentNode.querySelector('.custom-file-label');
+                    $label.innerHTML = $input.value;
+                });
+        }*/
+    }
+);
